beaa607dd3ef2a164d3330f8484028eda767167f
96krish <31807145+96krish@users.noreply.github.com>
2019-05-08 14:14:34 +0530

    Create pyramid.c

---
+++ pyramid.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main()
+{
+  int row, c, n, s;
+
+  printf(" number of rows in pyramid of stars you wish to see = 4\n");
+  //scanf("%d", &n);
+
+  s = 4;
+
+  for (row = 1; row <= n; row++)  // Loop to print rows
+  {
+    for (c = 1; c < s; c++)  // Loop to print spaces in a row
+      printf(" ");
+
+    s--;
+
+    for (c = 1; c <= 2*row - 1; c++) // Loop to print stars in a row
+      printf("*");
+
+    printf("\n");
+  }
+
+  return 0;
+}
